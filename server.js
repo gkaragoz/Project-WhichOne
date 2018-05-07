36b990d36d12ccb1c213683a854e8aa37393a566
gkaragoz <annotto3@gmail.com>
2018-05-08 01:11:03 +0300

    Init socket.io components

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ var express = require('express'),
     app = express(),
     port = process.env.PORT || 3000,
     path = require('path'),
+    http = require('http'),
     mongoose = require('mongoose'),
     Models = require('./api/models/whichOneModels'), //created model loading here
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    socketIO=require('socket.io'),
+    server = http.createServer(app);
+    io = socketIO(server);
 
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
@@ -24,4 +28,4 @@ routes(app); //register the route
 
 app.listen(port);
 
-console.log('WhichOne - RESTful API server started on: ' + port);
+console.log('WhichOne - RESTful API and Socket server started on: ' + port);
