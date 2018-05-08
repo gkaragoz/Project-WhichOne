e329f3d7502d716e46ce45a595cbfaa902de2640
oguzhanaydemir <ouzhanaydemir7@gmail.com>
2018-05-08 03:18:52 +0300

    Complete work on the interface

--- public/js/events.js
+++ public/js/events.js
@@ -4,35 +4,38 @@ $(document).ready(function () {
     var leftText = $("#left-text");
     var rightText = $("#right-text");
     var questionText = $("#question");
-    var leftImage = $("#left-image");
-    var rightImage = $("#right-image");
+    var leftDiv = $("#left-div");
+    var rightDiv = $("#right-div");
+    var details = $(".details-row");
     var leftVote = $("#left-vote");
     var rightVote = $("#right-vote");
     var leftPercent = $("#left-percent");
     var rightPercent = $("#right-percent");
+    var nextButton = $("#next-button");
+    var clickControl = true;
 
     getAQuestion(function (question) {
         handleQuestionResponse(question);
     });
 
-    $("#left-image").click(function () {
-        alert("Sol fotoğrafa tıklandı");
-
-        updateAQuestionVote(currentQuestion._id, currentQuestion.leftVotes, 'left');
+    leftDiv.click(function () {
+        hasClicked('#left-div', '#right-div', 'left');
     });
 
-    $("#right-image").click(function () {
-        alert("Sağ fotoğrafa tıklandı");
-
-        updateAQuestionVote(currentQuestion._id, currentQuestion.rightVotes, 'right');
+    rightDiv.click(function () {
+        hasClicked('#right-div', '#left-div', 'right');
     });
 
-    $("next-button").click(function () {
-        alert("Sonraki butonuna tıklandı");
+    nextButton.click(function () {
 
-        getAQuestion(function (question) {
-            handleQuestionResponse(question);
-        });
+        if (!clickControl) {
+            alert("İleri butonuna tıklandı");
+            /*getAQuestion(function (question) {
+                handleQuestionResponse(question);
+            });*/
+            setDefaultProperties();
+            clickControl = true;
+        }
     });
 
     function handleQuestionResponse(question) {
@@ -42,8 +45,8 @@ $(document).ready(function () {
         leftText.html(question.leftChoice.name);
         rightText.html(question.rightChoice.name);
         questionText.html(question.text);
-        leftImage.attr("src", question.leftChoice.imageUrl);
-        rightImage.attr("src", question.rightChoice.imageUrl);
+        leftDiv.attr("src", question.leftChoice.imageUrl);
+        rightDiv.attr("src", question.rightChoice.imageUrl);
         leftVote.html(question.leftVotes + " kez oylandı.");
         rightVote.html(question.rightVotes + " kez oylandı.");
 
@@ -53,5 +56,35 @@ $(document).ready(function () {
 
         leftPercent.html("%" + leftPercentage);
         rightPercent.html("%" + rightPercentage);
+    }
+
+    function setChoose(chooseContent, otherContent) {
+        $(chooseContent).css('background-color', 'rgba(0, 255, 0, .7)');
+        $(otherContent).css('background-color', 'rgba(255, 0, 0, .7)');
+        $(".details-row").css('display', 'block');
+    }
+
+    function setDefaultProperties(){
+        leftDiv.css('background-color', '')
+        rightDiv.css('background-color', '')
+        details.css('display', 'none');
+    }
+
+    function hasClicked(chooseSide, otherSide, way) {
+        
+        if (clickControl) {
+
+            setChoose(chooseSide, otherSide);
+
+            if (way === 'right') {
+                
+                //updateAQuestionVote(currentQuestion._id, currentQuestion.rightVotes, way);
+            }
+            else if (way === 'left') {
+                
+                //updateAQuestionVote(currentQuestion._id, currentQuestion.leftVotes, way);
+            }
+            clickControl = false;
+        }
     }
 });
